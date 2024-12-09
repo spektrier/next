1cff09e2652af33a57cd95d08dd658efc817c50d
Hugo Pereira - a22303324 <160623216+spektrier@users.noreply.github.com>
2024-12-09 13:27:02 +0000

    Header and Footer working

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,6 +1,8 @@
 import type { Metadata } from "next";
 import localFont from "next/font/local";
 import "./globals.css";
+import Header from "@/components/Header/Header";
+import Footer from "@/components/Footer/Footer";
 
 const geistSans = localFont({
   src: "./fonts/GeistVF.woff",
@@ -24,12 +26,13 @@ export default function RootLayout({
   children: React.ReactNode;
 }>) {
   return (
-    <html lang="en">
-      <body
-        className={`${geistSans.variable} ${geistMono.variable} antialiased`}
-      >
-        {children}
+      <html lang="pt">
+        <body>
+          <Header />
+          <main>{children}</main>
+          <Footer />
+      );
       </body>
-    </html>
+      </html>
   );
 }
